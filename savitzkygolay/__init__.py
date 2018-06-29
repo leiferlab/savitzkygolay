dd960c2e24cad1d63f0f9ed3fc183fd723aa372f
Francesco Randi <francesco.randi@gmail.com>
2018-06-29 11:08:42 -0400

    First fixes. Renaming functions to get_*_derivative() and creating dummy placeholders to get_*_filters() [For caching purposes, it may be better to have a class to perform the general filtering.]

--- savitzkygolay/__init__.py
+++ savitzkygolay/__init__.py
@@ -1,3 +1,3 @@
 __all__ = ['savitzkygolay']
 
-from . import savitzkygolay
+from .savitzkygolay import *
--- savitzkygolay/savitzkygolay.py
+++ savitzkygolay/savitzkygolay.py
@@ -1,29 +1,75 @@
 import numpy as np
+import pkg_resources
 
-def get_filter(mu, poly, order, d):
+def get_1D_filter():
+    return 0
+
+def get_3D_filter():
+    return 0
+
+def get_1D_derivative(mu, poly, order, d):
     '''Loads the Savitzky Golay filter of size mu with poly-th polynomial order
     and dimensionality d.'''
     
-    folder = 'filters/'
+    folder = pkg_resources.resource_filename('savitzkygolay', 'filters/')
     
-    # 1D filter
-    if d==1:
-        f = open(folder+'1DSavitzkyGolayFilters/CC_00'+str(mu)+'_00'+str(poly)+'.dat',\
-                'r')
+    f = open(folder+'1DSavitzkyGolayFilters/CC_00'+str(mu)+'_00'+str(poly)+'.dat',\
+            'r')
 
-        #Go through file
-        while 1:
-            s = f.readline()
-            if s=='# Matrix starts:\n':
-                break
-                
-        #Jump to coefficient you want to calculate
-        C = np.zeros((mu))
-        L = order #order-th derivative
-        for l in np.arange(L):
-            f.readline()
-            
+    #Go through file
+    while 1:
         s = f.readline()
+        if s=='# Matrix starts:\n':
+            break
+            
+    #Jump to coefficient you want to calculate
+    C = np.zeros((mu))
+    L = order #order-th derivative
+    for l in np.arange(L):
+        f.readline()
+        
+    s = f.readline()
+    s = s.split('\t')
+    symmetry = s[1]
+    s = np.array(s[2:]).astype(np.float)
+
+    #Populate second half based on symmetry (centred filters, leave central 
+    #col out)
+    if symmetry =='S':
+        s = np.append(s,s[0:-1][::-1])
+    elif symmetry == 'A':
+        s = np.append(s,-1.0*s[0:-1][::-1])
+    C = s
+    f.close()
+    
+    return C
+        
+def get_3D_derivative(mu, nu, rho, poly, order, d):
+
+    folder = 'filters/'
+
+    f = open('3DSavitzkyGolayFilters/CC_00'+str(mu)+'x00'+str(nu)+'x00'+ \
+        str(rho)+'_00'+str(poly)+'x00'+str(poly)+'x00'+str(poly)+'.dat','r')
+        
+    #Go through file
+    while 1:
+        s = f.readline()
+        if s=='# Matrix starts:\n':
+            break
+            
+    #Jump to coefficient you want to calculate
+    #k+j*nu+i*nu*rho with k,j,i=200,020,002
+    l0 = 2
+    l1 = 2*nu
+    l2 = 2*nu*rho
+
+    L = [l0,l1,l2]
+    q = len(L)
+    C = np.zeros((q,mu*nu*rho))
+    p = 0
+    S = f.readlines()
+    for l in L:
+        s = S[l]
         s = s.split('\t')
         symmetry = s[1]
         s = np.array(s[2:]).astype(np.float)
@@ -34,45 +80,8 @@ def get_filter(mu, poly, order, d):
             s = np.append(s,s[0:-1][::-1])
         elif symmetry == 'A':
             s = np.append(s,-1.0*s[0:-1][::-1])
-        C = s
-        f.close()
-        
-    # 3D filter
-    elif d==3:    
-        f = open('3DSavitzkyGolayFilters/CC_00'+str(mu)+'x00'+str(nu)+'x00'+ \
-            str(ro)+'_00'+str(poly)+'x00'+str(poly)+'x00'+str(poly)+'.dat','r')
-
-        #Go through file
-        while 1:
-            s = f.readline()
-            if s=='# Matrix starts:\n':
-                break
-                
-        #Jump to coefficient you want to calculate
-        #k+j*nu+i*nu*ro with k,j,i=200,020,002
-        l0 = 2
-        l1 = 2*nu
-        l2 = 2*nu*ro
-
-        L = [l0,l1,l2]
-        q = len(L)
-        C = np.zeros((q,mu*nu*ro))
-        p = 0
-        S = f.readlines()
-        for l in L:
-            s = S[l]
-            s = s.split('\t')
-            symmetry = s[1]
-            s = np.array(s[2:]).astype(np.float)
-
-            #Populate second half based on symmetry (centred filters, leave central 
-            #col out)
-            if symmetry =='S':
-                s = np.append(s,s[0:-1][::-1])
-            elif symmetry == 'A':
-                s = np.append(s,-1.0*s[0:-1][::-1])
-            C[p] = s
-            p += 1
-        f.close()
+        C[p] = s
+        p += 1
+    f.close()
         
     return C
